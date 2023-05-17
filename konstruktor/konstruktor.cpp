32716cb3e86f85e1ec060ae451c0afeee97fb684
veresoora <lahanabira@gmail.com>
2023-05-17 17:57:20 +0700

    mencetak nama dan nim mahasiswa

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -13,3 +13,26 @@ public:
 	mahasiswa(int iNim, string iNama);
 	void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
