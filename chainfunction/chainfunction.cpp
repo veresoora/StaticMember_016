326c41e3c1a1045a1ae458a7f5c52729c96f9b7a
veresoora <lahanabira@gmail.com>
2023-05-17 17:13:13 +0700

    membuat objek

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -15,3 +15,11 @@ public:
 		return this->judul;
 	}
 } bukunya;
+
+int main()
+{
+	bukunya.setJudul("Matematika");
+	cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+	return 0;
+}
